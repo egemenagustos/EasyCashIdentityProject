11d4adfe65e2ccfb10ff7ed9e70a0ff695133a4a
Egemen Ağustos <egemenagustos351903@gmail.com>
2023-05-04 11:31:53 +0300

    ders 5 sonu

--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -11,7 +11,7 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
         public int Id { get; set; }
 
         public string ProcessType { get; set; }
-
+        
         public decimal Amount { get; set; }
 
         public DateTime ProcessDate { get; set; }
