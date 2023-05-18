48a8bddb7b48dd5713757f6d1ba8710c3b705ace
egemenagustos <egemenagustos351903@gmail.com>
2023-05-18 16:07:57 +0300

    ders 16

--- EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,22 +1,35 @@
-﻿using EasyCashIdentityProject.PresentationLayer.Models;
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
     public class ConfirmMailController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult Index(int id)
         {
             var value = TempData["Mail"];
-            ViewBag.v = value + "aaa";
+            ViewBag.v = value;
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(ConfirmMailViewModel request)
+        public async Task<IActionResult> Index(ConfirmMailViewModel request)
         {
-
+            var mail = await _userManager.FindByEmailAsync(request.Mail);
+            if(mail.ConfirmCode == request.ConfirmCode)
+            {
+                return RedirectToAction("Index","MyProfile");
+            }
             return View();
         }
     }
--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -44,7 +44,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                 if (result.Succeeded)
                 {
                     MimeMessage mimeMessage = new();
-                    MailboxAddress mailboxAddress = new("Admin","sdsddsdsdssdds@gmail.com");
+                    MailboxAddress mailboxAddress = new("Admin", "dsdsdssdds@gmail.com");
                     MailboxAddress mailboxAddressTo = new("user",appUser.Email);
                     mimeMessage.From.Add(mailboxAddress);
                     mimeMessage.To.Add(mailboxAddressTo);
@@ -55,7 +55,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
 
                     SmtpClient client = new();
                     client.Connect("smtp.gmail.com",587,false);
-                    client.Authenticate("sddsdsdsdsds@gmail.com", "sdsdsdsddssdsdsddsdsk");
+                    client.Authenticate("sddssddsdsdsds@gmail.com", "dsdsddsdsdssdsd");
                     client.Send(mimeMessage);
                     client.Disconnect(true);
 
