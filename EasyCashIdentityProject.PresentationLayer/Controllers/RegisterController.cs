7f15190b36d5b6353fb7c4f06fb48501a70b47b7
egemenagustos <egemenagustos351903@gmail.com>
2023-06-01 15:20:06 +0300

    Ders 20 sonu

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -44,7 +44,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                 if (result.Succeeded)
                 {
                     MimeMessage mimeMessage = new();
-                    MailboxAddress mailboxAddress = new("Admin", "dfdfdfdf@gmail.com");
+                    MailboxAddress mailboxAddress = new("Admin", "egemenagustos351903@gmail.com");
                     MailboxAddress mailboxAddressTo = new("user",appUser.Email);
                     mimeMessage.From.Add(mailboxAddress);
                     mimeMessage.To.Add(mailboxAddressTo);
@@ -55,7 +55,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
 
                     SmtpClient client = new();
                     client.Connect("smtp.gmail.com",587,false);
-                    client.Authenticate("dffddffdfd@gmail.com", "fdfdfdfdfd");
+                    client.Authenticate("egemenagustos351903@gmail.com", "dfdffdfddfdfd");
                     client.Send(mimeMessage);
                     client.Disconnect(true);
 
